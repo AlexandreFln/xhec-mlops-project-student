ad6b0a907154077a8d705d06bf3d01a46fa5581e
alexandre_foulon <alexandre.foulon@hec.edu>
2024-10-24 15:32:15 +0200

    step 2 done

--- src/modelling/main.py
+++ src/modelling/main.py
@@ -1,19 +1,25 @@
-# This module is the training flow: it reads the data, preprocesses it, trains a model and saves it.
-
 import argparse
+from pathlib import Path
+
+import pandas as pd
+from preprocessing import encode_categorical_cols, extract_x_y
+from training import create_and_train_model
+from utils import save_pickle
 
 
 def main(trainset_path: Path) -> None:
     """Train a model using the data at the given path and save the model (pickle)."""
     # Read data
-
+    df = pd.read_csv(trainset_path)
     # Preprocess data
-
+    df = encode_categorical_cols(df)
+    x, y = extract_x_y(df)
     # (Optional) Pickle encoder if need be
-
     # Train model
+    model = create_and_train_model(x, y)
 
-    # Pickle model --> The model should be saved in pkl format the `src/web_service/local_objects` folder
+    # Pickle model --> Save the model in the pkl format in the specified folder
+    save_pickle("../web_service/local_objects", model)
 
 
 if __name__ == "__main__":
--- src/modelling/utils.py
+++ src/modelling/utils.py
@@ -1,1 +1,16 @@
 # Use this module to code a `pickle_object` function. This will be useful to pickle the model (and encoder if need be).
+import pickle
+from typing import Any
+
+
+def load_pickle(path: str):
+    """Load pickle object."""
+    with open(path, "rb") as f:
+        loaded_obj = pickle.load(f)
+    return loaded_obj
+
+
+def save_pickle(path: str, obj: Any):
+    """Save pickle object."""
+    with open(path, "wb") as f:
+        pickle.dump(obj, f)
